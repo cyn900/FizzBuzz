3b953db306ef60e49386ca942201a8094d43a396
cynthia <cyn.zhou@mail.utoronto.ca>
2023-09-11 20:00:35 -0400

    update to Task 3.4

---
+++ src/Multiples.java
@@ -0,0 +1,17 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 1; i < 1000; i++) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 || divisibleBy5) {
+                num++;
+            }
+        }
+        System.out.println(num);
+    }
+}
